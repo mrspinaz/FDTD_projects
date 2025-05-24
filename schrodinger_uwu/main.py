3de2f28904d2d8b898c25b9703bb3ae6dea8e593
mrspinaz <mrspinazze@uwaterloo.ca>
2025-05-23 21:59:49 -0400

    updated main with real and imag

--- schrodinger_uwu/main.py
+++ schrodinger_uwu/main.py
@@ -1,38 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt 
-import os 
 
 Nx = 301 #Number of spatial grid points
-Nt = int(5e2) #Number of temporal grid points
+Nt = int(1e4) #Number of temporal grid points
 hbar = 1.0
 L = 1.0 #Simulation box length 
 m = 1.0
 k = np.pi/L
 dx = L/(Nx - 1) 
 dt = 1e-7
+c1 = (hbar*dt)/(2*m*dx**2)
+c2 = dt/hbar
 
 #Initial Setup
 x = np.arange(0,L+dx,dx)
 psi0 = np.sqrt(2/L)*np.sin((np.pi/L)*x)
 psi0[0] = 0
 psi0[-1] = 0
-psi_mat = np.zeros([Nt,Nx],dtype='complex')
-psi_mat[0][:] = psi0
+psiR_mat = np.zeros([Nt,Nx],dtype='float')
+psiI_mat = np.zeros([Nt,Nx],dtype='float')
+psiR_mat[0][:] = psi0
+#psiI initially should be zeros for infinite well ground state.
 
 #Define potential profile (Gaussian)
 mu, sigma = 1.0/2.0, 1.0/20.0
-V = -1e4*np.exp(-(x-mu)**2/(2.0*sigma**2))
+V = x*0 #-1e4*np.exp(-(x-mu)**2/(2.0*sigma**2))
 
 for t in range(0,Nt-1):
     for i in range(1,Nx-1):
-        psi_mat[t+1][i] = psi_mat[t][i] + (1j/2)*(dt/dx**2)*(psi_mat[t][i+1] - 2*psi_mat[t][i] + psi_mat[t][i-1]) - 1j*dt*V[i]*psi_mat[t][i]
+        psiI_mat[t+1][i] =  c1*(psiR_mat[t][i+1] - 2*psiR_mat[t][i] + psiR_mat[t][i-1]) - c2*V[i]*psiR_mat[t][i] + psiI_mat[t][i]
+        psiR_mat[t+1][i] =  -c1*(psiI_mat[t+1][i+1] - 2*psiI_mat[t+1][i] + psiI_mat[t+1][i-1]) + c2*V[i]*psiI_mat[t+1][i] + psiR_mat[t][i]
 
-    normal = np.sum(np.abs(psi_mat[t+1]**2))*dx
+    normal = np.sum(psiR_mat[t+1][i]**2 + psiI_mat[t+1][i]**2)*dx
     #normalize 
-    psi_mat[t+1] /= np.sqrt(normal) 
+    psiR_mat[i+1] /= np.sqrt(normal)
+    psiI_mat[t+1] /= np.sqrt(normal) 
 
-path_psi_mat = os.path.abspath("")
-np.save('schrodinger_uwu/psi_mat.npy', psi_mat)
+np.save('schrodinger_uwu/psiR_mat.npy', psiR_mat)
+np.save('schrodinger_uwu/psiI_mat.npy', psiI_mat)
 np.save('schrodinger_uwu/x_coords.npy', x)
 np.save('schrodinger_uwu/potential.npy', V)
 
--- schrodinger_uwu/postprocess.py
+++ schrodinger_uwu/postprocess.py
@@ -9,24 +9,31 @@ axis = plt.axes(xlim =(0, 1),
 
 line1, = axis.plot([], [])
 line2, = axis.plot([], [])
+line3, = axis.plot([], [])
 
 
+
+psiR_mat = np.load('schrodinger_uwu/psiR_mat.npy')
+psiI_mat = np.load('schrodinger_uwu/psiI_mat.npy')
 V = np.load('schrodinger_uwu/potential.npy')
-psi_mat = np.load('schrodinger_uwu/psi_mat.npy')
 x = np.load('schrodinger_uwu/x_coords.npy')
 
 def init():
     line1.set_data([], [])
     line2.set_data([], [])
-    return line1, line2,
+    line3.set_data([], [])
+    return line1, line2, line3,
 
 def animate(i):
-    y = np.abs(psi_mat[i*10][:])**2
-    line1.set_data(x,y)
-    line2.set_data(x,V*1e-4)
-    return line1, line2,
+    y1 = psiR_mat[i*10][:]
+    y2 = psiI_mat[i*10][:]
 
-anim = FuncAnimation(fig, animate, init_func=init, frames=5000, interval=1, blit=True)
+    line1.set_data(x,y1)
+    line2.set_data(x,V*1e-4)
+    line3.set_data(x,y2)
+    return line1, line2, line3,
+
+anim = FuncAnimation(fig, animate, init_func=init, frames=1000, interval=1, blit=True)
 plt.show()
 writer = PillowWriter(fps=30)
 anim.save('schrodinger_uwu/test.gif', writer = writer)
